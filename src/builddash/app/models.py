2f3945bfd4745b0c8e528d152f636312d03f080c
Tom Wardill <tom@howrandom.net>
2010-06-18 11:56:54 +0100

    Added a database for caching the JSON files

--- src/builddash/app/models.py
+++ src/builddash/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class JSONCache(models.Model):
+    """ Cache the JSON that we get from the buildbot server, and add a time """
+    main_json = models.TextField()
+    builders_json = models.TextField()
+    retrieval_time = models.DateTimeField()
