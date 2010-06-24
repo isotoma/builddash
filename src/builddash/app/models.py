89b5577beb8bf2116e8b790345667aadd0b53842
Tom Wardill <tom@howrandom.net>
2010-06-24 09:53:36 +0100

    Unmerged models

--- src/builddash/app/models.py
+++ src/builddash/app/models.py
@@ -2,12 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class MainCache(models.Model):
-    time_stamp = models.DateTimeField()
-    json = models.TextField()
-
-class BuilderCache(models.Model):
-    parent = models.ForeignKey(MainCache)
-    json = models.TextField()
-    builder = modelsTextField()
-
