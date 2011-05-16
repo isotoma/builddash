646147547809ad8912ec677f2e357b889a5a7a77
Gary <gjm@isotoma.com>
2011-05-16 15:34:03 +0100

    refactoring to give ordered lists and remove build categories

--- src/builddash/app/views.py
+++ src/builddash/app/views.py
@@ -19,54 +19,63 @@ def view(request):
     categories = {}
     for k,z in loaded_data['builders'].iteritems():
         
+        builder = k
         category = z['category']
-        if isinstance(category, dict):
-            continue
-        if not categories.has_key(category) :
-            categories[category] = {'state': '', 'builders': {}, }
         
         if z['cachedBuilds']:
+            # there are previous builds
             last_build_number = z['cachedBuilds'][-1:][0]
-            build_info = urllib.urlopen(settings.BUILDBOT_URL + '/json/builders/' + k + '/builds/' + str(last_build_number)).read()
+            build_info = urllib.urlopen(settings.BUILDBOT_URL + 
+                                        '/json/builders/' + k + '/builds/' + 
+                                        str(last_build_number)).read()
             parsed_build_info = json.loads(build_info)
+            
             if parsed_build_info['text']:
-                status = parsed_build_info['text'][0]
+                last_build_status = parsed_build_info['text'][0]
             else:
-                status = 'building'
+                last_build_status = 'building'
+            
             if len(parsed_build_info['text']) > 1:
-                text = ''.join(parsed_build_info['text'][1:])
-                
-            time_finish_build =time.strftime("%d-%m-%y %H:%M", time.localtime(parsed_build_info['times'][1]))
+                extra_text = ''.join(parsed_build_info['text'][1:])
+            else:
+                extra_text = ''
             
-            if z['state'] == 'building':
-                categories[category]['state'] = 'building'
-            if status == 'exception' and categories[category]['state'] != 'building':
-                categories[category]['state'] = status
-            elif status == 'failed' and categories[category]['state'] != 'exception' and categories[category]['state'] != 'building':
-                categories[category]['state'] = status
-            elif status == 'build' and categories[category]['state'] != 'failed' and categories[category]['state'] != 'exception' and categories[category]['state'] != 'building':
-                categories[category]['state'] = status
-                
-            categories[category]['builders'][k] = {'status':status, 'time': time_finish_build}
-                
+            finish_time = time.localtime(parsed_build_info['times'][1])
+            time_finish_build = time.strftime("%Y-%m-%d %H:%M", finish_time)
             
-
+            current_state = z['state']
+            if len(z['pendingBuilds']):
+                status = 'building'
+            elif current_state in ['building','exception','failed','offline']:
+                status = current_state
+            else:
+                status = last_build_status
+            
+            if current_state == 'offline' and status == 'building':
+                status = 'offline'
+            
+            if status not in categories.keys():
+                categories[status] = {'state': status,
+                                      'builders': {},
+                                     }
+            categories[status]['builders'][k] = {'status': status,
+                                                 'time': time_finish_build,
+                                                 'category': category,
+                                                 'builder': k,
+                                                 'pending':len(z['pendingBuilds']),
+                                                }
+            
     
-    seperated_cats = {'building': [], 'build': [], 'failed': [], 'exception': []}
-    
+    separated_cats = {'building': [], 'offline': [], 'build': [], 'failed': [], 'exception': []}
     for c in categories.keys():
-        
-        if categories[c]['state'] == 'building':
-            seperated_cats['building'].append({c:categories[c]})
-        if categories[c]['state'] == 'build':
-            seperated_cats['build'].append({c:categories[c]})
-        if categories[c]['state'] == 'failed':
-            seperated_cats['failed'].append({c:categories[c]})
-        if categories[c]['state'] == 'exception':
-            seperated_cats['exception'].append({c:categories[c]})
+        state = categories[c]['state']
+        if state in separated_cats.keys():
+            builders = sorted(categories[c]['builders'].values(),
+                              key=lambda k: k['time'], reverse=True)
             
+            separated_cats[state].extend(builders)
     
-    categories_to_send = json.dumps(seperated_cats)
+    categories_to_send = json.dumps(separated_cats)
     
     return HttpResponse(categories_to_send)
 
--- src/builddash/static/js/dashboard.js
+++ src/builddash/static/js/dashboard.js
@@ -1,4 +1,7 @@
 var timeout = 10000;
+var column_height = 10;
+var max_build_category = 5;
+
 $.ajaxSetup({
     url: 'view/',
     success: do_layout,
@@ -19,24 +22,24 @@ function update()
 var div_to_insert;
 
 function insert_to_page(i, item) {
-
-        for (x in item)
-        {
-            var item_html = "<td class='build-item'>";
-            
-            item_html += "<h2><a href=" + buildbot_url + "/waterfall?category=" + x + ">" + x + "</a></h2>";
-            
-            item_html += "<ul>";
-            $.each(item[x].builders, function (j, builder) {
-                item_html += "<li ><div><img src='static/img/"+ builder['status'] + ".png' /><a href=" + buildbot_url + "/builders/" + j +">" + j + "</a></div><span class='state '>" + builder['status'] + "</span><span class='time'>" + builder['time'] + "</span></li>";            
-            });
-            item_html += "</ul>";
-            
-            item_html += "</td>";
-            div_to_insert.append($(item_html));
-            break;
-        }
+    var item_html = "<td class='build-item'>";
     
+    item_html += "<h2><a href=" + buildbot_url + "/waterfall?category=" + item.category + ">" + item.category + "</a></h2>";
+    
+    item_html += "<ul>";
+    $.each(item.builders, function (j, builder) {
+        var category = builder['category'];
+        var category_link = "<a href=" + buildbot_url + "/waterfall?category=" + category + ">" + category + "</a>";
+        var build_time = builder['time'];
+        var icon_html = "<img src='static/img/"+ builder['status'] + ".png' />";
+        var icon_html = "<img src='static/img/"+ builder['status'] + ".png' />";
+        
+        item_html += "<li ><div>"+icon_html+"<a href=" + buildbot_url + "/builders/" + builder.builder +">" + builder.builder + "</a></div><span class='state '>" + builder['status'] + "</span><span class='time'>" + builder['time'] + "</span><span class='category'>" + category_link + "</span></li>";
+    });
+    item_html += "</ul>";
+    
+    item_html += "</td>";
+    div_to_insert.append($(item_html));
 }
 
 function insert_build_to_page(i, item) {
@@ -59,24 +62,47 @@ function next() {
     window.setTimeout(update, timeout);
 }
 
+function columnify_data(data,title) {
+    var category = [];
+    var full_title = title;
+    var max_i = Math.ceil(data.length/column_height);
+    for (var i = 0; i < max_i; i++){
+        var j = i + 1;
+        if (max_i > 1) full_title = title +  ' ' + j + '/' + max_i;
+        category.push({'category' : full_title,
+                       'builders' : data.slice(i*column_height,j*column_height)});
+    }
+    return category;
+}
 function do_layout(json) {
     if (json) {
+        var building = columnify_data(json.building,'Building');
+        var offline = columnify_data(json.offline,'Offline (Building or Pending)');
+        var exception = columnify_data(json.exception,'Exception');
+        var failed = columnify_data(json.failed,'Failed');
+        var build = columnify_data(json.build.slice(0,max_build_category),'Last ' + max_build_category + ' successful builds (total = ' + json.build.length + ')');
+                        
         $('body').removeClass('loading');
+        
         div_to_insert = $('#building');
         div_to_insert.children().remove();
-        $.each(json.building, insert_to_page);
+        $.each(building, insert_to_page);
 
+        div_to_insert = $('#offline');
+        div_to_insert.children().remove()
+        $.each(offline, insert_to_page);
+        
         div_to_insert = $('#exception');
         div_to_insert.children().remove()
-        $.each(json.exception, insert_to_page);
+        $.each(exception, insert_to_page);
 
         div_to_insert = $('#failed');
         div_to_insert.children().remove()
-        $.each(json.failed, insert_to_page );
+        $.each(failed, insert_to_page );
 
         div_to_insert = $('#build');
         div_to_insert.children().remove()
-        $.each(json.build, insert_build_to_page );
+        $.each(build, insert_to_page );
     }
     
     next();
